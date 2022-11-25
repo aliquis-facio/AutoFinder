a0d58bddf8ded59a24d2a2effa8d074d8c53ec2d
jenych0314 <jeonych305@gmail.com>
2022-11-25 18:51:20 +0900

    own의 경우, 형용사와 대명사가 같은 줄에 쓰여 있어서, 태그에 대명사가 무시당했다.

--- StringManage.py
+++ StringManage.py
@@ -132,7 +132,7 @@ class Formatter:
 
 
 if __name__ == "__main__":
-    input_word_lst = ["drive", "on the contrary"]
+    input_word_lst = ["drive", "on the contrary", "own"]
 
     ChromeDriver = Crawling()
     fomatter = None
